fc3c60d3b03ec92aeff3f87fb2d1ad29eed24b2b
jiabhullar <jiakbhullar@gmail.com>
2025-11-29 18:10:06 +0000

    Add ProductTile skeleton

--- lib/collection_page.dart
+++ lib/collection_page.dart
@@ -52,3 +52,29 @@ class CollectionPage extends StatelessWidget {
     );
   }
 }
+
+class _ProductTile extends StatelessWidget {
+  final String title;
+  final String price;
+  final String imageUrl;
+
+  const _ProductTile({
+    super.key,
+    required this.title,
+    required this.price,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // image will be added later
+        Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+        const SizedBox(height: 4),
+        Text(price, style: const TextStyle(color: Colors.grey)),
+      ],
+    );
+  }
+}
