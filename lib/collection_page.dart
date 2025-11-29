92f1caa4517fa7782ef083a452e23794443a2a1d
jiabhullar <jiakbhullar@gmail.com>
2025-11-29 17:39:10 +0000

    Create empty scaffold

---
+++ lib/collection_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CollectionPage extends StatelessWidget {
+  final String collectionName;
+
+  const CollectionPage({super.key, required this.collectionName});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(); 
+  }
+}
