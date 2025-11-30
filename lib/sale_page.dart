2c226abca6cf5737b8d35470560186d703292b89
jiabhullar <jiakbhullar@gmail.com>
2025-11-30 14:56:12 +0000

    Add baner container and text

--- lib/sale_page.dart
+++ lib/sale_page.dart
@@ -10,6 +10,13 @@ class SalePage extends StatelessWidget {
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: [],
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(16.0),
+            color: Colors.redAccent,
+            child: Text(
+              'Sale Page - Coming Soon!',
+          )
       ),
       ),
     );
