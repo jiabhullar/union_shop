308682b3542c3f6faf1f50823cc2cb4e2283a476
jiabhullar <jiakbhullar@gmail.com>
2025-11-30 18:18:24 +0000

    Add tags

--- lib/data/sample_products.dart
+++ lib/data/sample_products.dart
@@ -4,17 +4,21 @@ final List<Product> sampleProducts = [
   Product(
       title: "Campus Hoodie",
       price: "£29.99",
-      imageUrl: "https://via.placeholder.com/300?text=Hoodie"),
+      imageUrl: "https://via.placeholder.com/300?text=Hoodie",
+      tag: "new",),
   Product(
       title: "Union Mug",
       price: "£9.99",
-      imageUrl: "https://via.placeholder.com/300?text=Mug"),
+      imageUrl: "https://via.placeholder.com/300?text=Mug",
+      tag: "sale",),
   Product(
       title: "Notebook",
       price: "£4.99",
-      imageUrl: "https://via.placeholder.com/300?text=Notebook"),
+      imageUrl: "https://via.placeholder.com/300?text=Notebook",
+      tag: "sale",),
   Product(
       title: "Lanyard",
       price: "£2.99",
-      imageUrl: "https://via.placeholder.com/300?text=Lanyard"),
+      imageUrl: "https://via.placeholder.com/300?text=Lanyard",
+      tag: "sale",),
 ];
--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,11 +3,13 @@ class Product {
   final String price;
   final String imageUrl;
   final bool onSale;
+  final String tag;
 
   Product({
     required this.title,
     required this.price,
     required this.imageUrl,
-    this.onSale = false,
+    this.onSale = false,  
+    required this.tag,
   });
 }
