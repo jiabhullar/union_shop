408bdc8bd6a28f760fbd81708c553afe42c7aee9
jiabhullar <jiakbhullar@gmail.com>
2025-11-30 16:14:29 +0000

    Add welcome text and styling

--- lib/auth_page.dart
+++ lib/auth_page.dart
@@ -10,6 +10,28 @@ class AuthPage extends StatelessWidget {
         title: const Text('Sign In / Register'),
         backgroundColor: const Color(0xFF4d2963),
       ),
-      
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+           Text(
+              'Welcome to The Union Shop! Please sign in or register to continue.',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Sign in to your account',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
