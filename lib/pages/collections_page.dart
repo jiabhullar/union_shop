b469362f540fedafd13c5ea74d3344a0c7feb6e8
jiabhullar <jiakbhullar@gmail.com>
2025-11-30 17:33:58 +0000

    Delete inline productcard class

--- lib/pages/collections_page.dart
+++ lib/pages/collections_page.dart
@@ -3,56 +3,6 @@ import 'package:union_shop/widgets/nav_bar.dart';
 import 'package:union_shop/models/product.dart';
 import 'package:union_shop/data/sample_products.dart';
 
-
-class ProductCard extends StatelessWidget {
-  final String title;
-  final String price;
-  final String imageUrl;
-
-  const ProductCard({
-    super.key,
-    required this.title,
-    required this.price,
-    required this.imageUrl,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          height: 150, 
-          width: double.infinity,
-          child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
-          errorBuilder: (context, error, stackTrace) {
-            return Container(
-              color: Colors.grey[200],
-              child: const Center(
-              child:  Icon(Icons.image_not_supported, color: Colors.grey),
-            ),
-          );
-        },
-      ),
-    ),
-        const SizedBox(height: 8),
-        Text(
-          title,
-          style: const TextStyle(fontSize: 14, color: Colors.black),
-          maxLines: 2,
-        ),
-        const SizedBox(height: 4),
-        Text(
-          price,
-          style: const TextStyle(fontSize: 13, color: Colors.grey),
-        ),
-      ],
-    );
-  }
-}
-
 class CollectionsPage extends StatefulWidget {
   const CollectionsPage({super.key});
 
