565f44f98633058060ebcf4809b37464f1d9a8df
jiabhullar <jiakbhullar@gmail.com>
2025-11-30 16:07:36 +0000

    Add add bar and styling

--- lib/auth_page.dart
+++ lib/auth_page.dart
@@ -5,6 +5,11 @@ class AuthPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In / Register'),
+        backgroundColor: const Color(0xFF4d2963),
+      ),
+      
   }
 }
