88018daa48453c013c76392ba8c39fd5f94b75bd
jiabhullar <jiakbhullar@gmail.com>
2025-12-02 14:17:54 +0000

    Create stateful widget

---
+++ lib/pages/personalisation_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PersonalisationPage extends StatefulWidget {
+  const PersonalisationPage({super.key});
+
+  @override
+  State<PersonalisationPage> createState() => _PersonalisationPageState();
+}
