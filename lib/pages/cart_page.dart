17705285511df00c1240c2d63657f86cfadc46bd
jiabhullar <jiakbhullar@gmail.com>
2025-12-02 13:50:17 +0000

    Create cart page stateful widget

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,3 +2,10 @@ import 'package:flutter/material.dart';
 import '../widgets/nav_bar.dart';
 import '../services/cart_service.dart';
 import '../models/cart_item.dart';
+
+class CartPage extends StatefulWidget {
+  const CartPage({super.key});
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
