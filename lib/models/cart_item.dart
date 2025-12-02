4b5d1636985db499c8d19642bdd2bda6a819a0ca
jiabhullar <jiakbhullar@gmail.com>
2025-12-02 12:34:45 +0000

    Store product, quantity, and size

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -1,3 +1,13 @@
 import 'package:union_shop/models/product.dart';
 
-class CartItem {}
+class CartItem {
+  final Product product;
+  int quantity;
+  String size;
+
+  CartItem({
+    required this.product,
+    this.quantity = 1,
+    this.size = "M",
+  });
+}
