b38d38347d8dd38889edf381aa6e2ba6bdfd5138
jiabhullar <jiakbhullar@gmail.com>
2025-12-02 14:21:00 +0000

    Add build method

--- lib/pages/personalisation_page.dart
+++ lib/pages/personalisation_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:union_shop/widgets/nav_bar.dart';
 
 class PersonalisationPage extends StatefulWidget {
   const PersonalisationPage({super.key});
@@ -6,3 +7,17 @@ class PersonalisationPage extends StatefulWidget {
   @override
   State<PersonalisationPage> createState() => _PersonalisationPageState();
 }
+
+class _PersonalisationPageState extends State<PersonalisationPage> {
+  String selectedProductType = "T-Shirt";
+  String textInput = "";
+  String colour = "Black";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: 
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
