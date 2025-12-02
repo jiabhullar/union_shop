1289fa463cd7932676c3601ac56673cfad7d0fa6
jiabhullar <jiakbhullar@gmail.com>
2025-12-02 12:42:30 +0000

    Ensure only one instance of the cart exists across the app

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -1,4 +1,8 @@
 import 'package:union_shop/models/product.dart';
 import 'package:union_shop/models/cart_item.dart';  
 
-class CartService { }
+class CartService { 
+  CartService._privateConstructor();
+static final CartService _instance = CartService._privateConstructor();
+static CartService get instance => _instance;
+}
