85b439cf6c733264e8145a742337f7e99f38ee2c
jiabhullar <jiakbhullar@gmail.com>
2025-12-02 12:43:50 +0000

    Add internal cart storage

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -5,4 +5,7 @@ class CartService {
   CartService._privateConstructor();
 static final CartService _instance = CartService._privateConstructor();
 static CartService get instance => _instance;
+
+final List<CartItem> _items = [];
+List<CartItem> get items => _items;
 }
