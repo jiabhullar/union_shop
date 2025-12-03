e486071b2e06dd10bf549d9b46affd73ae072cfc
jiabhullar <jiakbhullar@gmail.com>
2025-12-03 10:38:58 +0000

    Create personalisaton about page

---
+++ lib/pages/personalisation_about_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:union_shop/widgets/nav_bar.dart';
+import 'package:union_shop/widgets/footer.dart';
+
+class PersonalisationAboutPage extends StatelessWidget {
+  const PersonalisationAboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold()
+  }
+}
