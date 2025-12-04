2ff76705030e8a10919bad381ce1d62578a8c85c
jiabhullar <jiakbhullar@gmail.com>
2025-12-04 15:19:46 +0000

    create product card test file

---
+++ test/product_card_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/models/product.dart';
+import 'package:union_shop/widgets/product_card.dart';
+import 'package:union_shop/pages/product_page.dart';
